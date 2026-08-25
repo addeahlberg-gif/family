4aebff1e42052e054fead5f1366ba93de0a32c06
addeahlberg-gif <addeahlberg@gmail.com>
2026-08-25 22:07:19 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,6 @@
-import{defineConfig}from'vite';import react from'@vitejs/plugin-react';import tailwind from'@tailwindcss/vite';export default defineConfig({plugins:[react(),tailwind()]});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()]
+});
